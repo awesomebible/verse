24b67d16fe4cd221d929c0940f8db21aea8ce521
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-08-26 15:55:19 +0200

    Update

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 	$DayOfYear = date('z') + 1;
-	$cacheRefFilePath = "cacheFile.txt"; // Edit this to the exact file path for your install of verse + "cacheFile.txt"
-	$cacheRef = file_get_contents("cacheFile.txt"); // Edit this to the exact file path for your install of verse + "cacheFile.txt"
-	$cachedImage = file_get_contents("cachedImage.jpg"); // Edit this to the exact file path for your install of verse + "cachedImage.jpg"
+	$cacheRef = "cacheFile.txt"; // Edit this to the exact file path for your install of verse + "cacheFile.txt"
+	$cachedImage = "cachedImage.jpg"; // Edit this to the exact file path for your install of verse + "cachedImage.jpg"
 
-		if($cacheRef == $DayOfYear){
+		if(file_get_contents($cacheRef) == $DayOfYear){
 			// If cache is fresh
 			header('Content-type: image/jpg;');
 			header('Cache-Control: max-age 86400');
-			echo $cachedImage;
+
+			echo file_get_contents($cachedImage);
 			die;
 		}else{
 			// If cache is stale
-			copy('https://raw.githubusercontent.com/awesomebible/verse/master/img/'.$DayOfYear.'.jpg', $cacheRefFilePath);
+			copy('https://raw.githubusercontent.com/awesomebible/verse/master/img/'.$DayOfYear.'.jpg', $cachedImage);
 
 			header('Content-type: image/jpg;');
 			header('Cache-Control: max-age 86400');
-			echo $cachedImage;
+			echo file_get_contents($cachedImage);
 
-			$myfile = fopen($cacheRefFilePath, "w") or die("Unable to open file!");
+			$myfile = fopen($cacheRef, "w") or die("Unable to open file!");
 			$txt = "".$DayOfYear."\n";
 			fwrite($myfile, $txt);
 			fclose($myfile);
