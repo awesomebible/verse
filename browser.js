79545b2074b20457f27706762ddf6b23d9c42106
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-08-26 16:05:28 +0200

    fixed browser.js

--- browser.js
+++ browser.js
@@ -1,3 +1,9 @@
 const AddToImage = "awb-verse" // Replace with the id of the img-tag
 
-document.getElementById(AddToImage).src = "https://verse.awesomebible.de"; 
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
+
+document.getElementById(AddToImage).src = "https://raw.githubusercontent.com/awesomebible/verse/master/img/"+day+".jpg; 
