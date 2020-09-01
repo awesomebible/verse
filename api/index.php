ee5005a207e061b597d3511bdb33faff23ca660c
Benjamin Wölkchen <bwolkchen@gmail.com>
2020-09-01 16:17:24 +0200

    Upload

--- api/index.php
+++ api/index.php
@@ -6,16 +6,21 @@ $keys_array = json_decode($keys_json);
 
 // Check if the API-Key is in keys.json
 if (in_array($api_key, $keys_array)) {
-    return true;
+    // Get location
+    $location = $_GET['location'];
+    $imagelist = file_get_contents("https://raw.githubusercontent.com/awesomebible/verse/master/api/imagelist.json");
+    $imagelist_json = json_decode($imagelist);
+
+    if (property_exists($imagelist_json, $location)){
+        $url = $imagelist_json->$location;
+
+        header('Content-type: image/jpg;');
+		header('Cache-Control: max-age 86400');
+        echo file_get_contents($url);
+        die;
+    }
 }else{
+    echo "invalid API-Key. Check again, or register one at: https://awesomebible.de/kontakt/";
     die;
 };
-
-// Get location
-$location = $_GET['location'];
-$imagelist = file_get_contents("https://raw.githubusercontent.com/awesomebible/verse/master/api/imagelist.json");
-
-if (property_exists($imagelist, $location)){
-    echo "treu";
-}
 ?>
