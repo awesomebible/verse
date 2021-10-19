89e4e163900f54e28f6fccad987e1980c29148be
Benjamin H <hbenjamin@tutanota.de>
2021-10-19 19:09:07 +0200

    Fix API Syntax & Error Messages

--- api/index.php
+++ api/index.php
@@ -1,36 +1,30 @@
 <?php
-    // Get location
-    $location = $_GET['location'];
-    $output = $_GET['output'];
+    if(isset($_GET['key']) && isset($_GET['location'])){
+        // Check if api key is valid
+        $key = $_GET['key'];
+        $keylist = json_decode(file_get_contents("keys.json"));
+        if(!in_array($key, $keylist)){
+            header("Content-Type: application/json;");
+            echo('{"status":"400", "content":"Invalid API-Key. Check again, or register one at https://awesomebible.de/kontakt/."}');
+            die;
+        }
+        if(isset($_GET['location'])){
+            $location = $_GET['location'];
+            $imagelist = json_decode(file_get_contents("https://codeberg.org/awesomeBible/verse/raw/branch/main/api/v1/".$location.".json"));
+            
+            $url = $imagelist->url;
 
-
-    if(isset($_GET['version'])){
-        $version = $_GET['version'];
-    };
-
-    $imagelist = file_get_contents("https://codeberg.org/awesomeBible/verse/raw/branch/master/api/v1/".$location.".json");
-    $imagelist_json = json_decode($imagelist);
-
-    $url = $imagelist_json->$version;
-    if (property_exists($imagelist_json, $version)){
-    if($output == "image"){
-        header('Content-type: image/jpeg;');
-        header('Cache-Control: max-age 31536000');
-        echo file_get_contents($url);
-        die;
-    }else{
-        header('Content-type: application/json;');
-        header('Cache-Control: max-age 31536000');
-        echo '{"status":"200", "content":"'.$url.'"}';
-        die;
+            if($_GET['output'] == "image"){
+                header('Content-type: image/jpg;');
+			    header('Cache-Control: max-age=86400');
+                print($url);
+            }else{
+                header('Content-type: application/json;');
+                echo '{"status":"200", "content":"'.$url.'"}';
+            }
+        }
     }
-}else{
     header('Content-type: application/json;');
     echo '{"status":"404", "content":"You probably didnt specify any parameters."}';
-    die; 
-}else{
-    header('Content-type: application/json;');
-    echo '{"status":"400", "content":"Invalid API-Key. Check again, or register one at https://awesomebible.de/kontakt/."}';
     die;
-}
 ?>
