7bb2e8820dc1c89d86ff66a67d3f6d923a86a527
Benjamin Hartmann <hbenjamin@tutanota.de>
2022-07-21 21:36:21 +0200

    Browser.js Jahrberechnung gefixt

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@ var now = new Date();
 var start = new Date(now.getFullYear(), 0, 0);
 var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
 var oneDay = 1000 * 60 * 60 * 24;
-var year = start.getFullYear()
+var year = now.getFullYear()
 var day = Math.floor(diff / oneDay);
 
 
