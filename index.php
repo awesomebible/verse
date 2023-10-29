535909d5faab898630915292a6e02d2afcba8258
Benjamin <benjamin@awesomebible.de>
2023-10-29 16:03:23 +0100

    Unterstützung für avif hinzugefügt

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 	$DayOfYear = date('z') + 1;
     $Year = date("Y");
-    $fileExtension = ".jpg"; // The file extension of the images with leading dot.
-    $fileMimeType = "image/jpg"; // The mime type of the image files, look here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    if(strpos( $_SERVER['HTTP_ACCEPT'], 'image/avif' ) !== false){
+        $fileExtension = ".avif"; // The file extension of the images with leading dot.
+        $fileMimeType = "image/avif"; // The mime type of the image files, look here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types    
+    } else {
+        $fileExtension = ".jpg"; // The file extension of the images with leading dot.
+        $fileMimeType = "image/jpg"; // The mime type of the image files, look here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types    
+    }
     $baseURL = "https://verse.awesomebible.de/img/"; # URL that points to the root of the image directory, with a trailing slash.
 	$cacheRef = "cacheFile.txt"; // Edit this to the exact file path for your install of verse + "cacheFile.txt"
-    $cachedImage = "cachedImage.jpg"; // Edit this to the exact file path for your install of verse + "cachedImage.fileExt"
+    $cachedImage = "cachedImage"; // Edit this to the exact file path for your install of verse + "cachedImage"
 
     function cacheStale(){
         global $Year;
@@ -17,13 +22,13 @@
         global $fileMimeType;
 
         // If cache is stale
-			if(file_exists($cachedImage)){unlink($cachedImage);};
+			if(file_exists($cachedImage.$fileExtension)){unlink($cachedImage.$fileExtension);};
 			if(file_exists($cacheRef)){unlink($cacheRef);};
 
-			copy($baseURL.$Year."/".$DayOfYear.$fileExtension, $cachedImage);
+			copy($baseURL.$Year."/".$DayOfYear.$fileExtension, $cachedImage.$fileExtension);
 
             header('Content-type: '.$fileMimeType.';');
-			echo file_get_contents($cachedImage);
+			echo file_get_contents($cachedImage.$fileExtension);
 
 			$CacheFileW = fopen($cacheRef, "w") or die("Unable to open file!");
 			$txt = "".$DayOfYear."\n";
@@ -36,8 +41,8 @@
         global $fileMimeType;
         	// If cache is fresh
 			header('Content-type: '.$fileMimeType.';');
-            if(file_exists($cachedImage)){
-			echo file_get_contents($cachedImage);
+            if(file_exists($cachedImage.$fileExtension)){
+			echo file_get_contents($cachedImage.$fileExtension);
             die;
         }else{
             cacheStale();
@@ -50,8 +55,8 @@
         cacheStale();
     }
 
-    echo "Sorry, but my creator forbid me to do that.";
+    echo "Oops, something went wrong. :(";
     echo "<br>";
-    echo "Error-Code: ".$DayOfYear."";
+    echo "Please contact ";
     die();
 ?>
